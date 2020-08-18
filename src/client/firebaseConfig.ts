9ba527e8a394b9387a32959b6d77b1a42b2d620c
totetero <totetero@gmail.com>
2020-08-18 23:58:51 +0900

    firebase init

---
+++ src/client/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export default {
+	apiKey: "xxxxxxxx",
+	authDomain: "xxxxxxxx",
+	databaseURL: "xxxxxxxx",
+	projectId: "fuhaha-train-view",
+	storageBucket: "xxxxxxxx",
+	messagingSenderId: "xxxxxxxx",
+	appId: "xxxxxxxx",
+};
