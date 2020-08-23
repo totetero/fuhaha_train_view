0798da23efde23f15681e29436b2712c0f901a51
totetero <totetero@gmail.com>
2020-08-23 13:38:18 +0900

    デプロイしたらfunctionが使えなかったので使えるように修正

--- hosting/src/component/main/apolloClient.ts
+++ hosting/src/component/main/apolloClient.ts
@@ -10,7 +10,11 @@ import { HttpLink, } from "apollo-link-http";
 export const apolloClient = new ApolloClient({
 	cache: new InMemoryCache(),
 	link: new HttpLink({
-		uri: "http://localhost:5001/fuhaha-train-view/us-central1/api/graphql",
+		uri: ((): string => {
+			if (process.env.NODE_ENV === "development") { return "http://localhost:5001/fuhaha-train-view/us-central1/api/graphql"; }
+			if (process.env.NODE_ENV === "production") { return "https://us-central1-fuhaha-train-view.cloudfunctions.net/api/graphql"; }
+			return "";
+		})(),
 	}),
 });
 
